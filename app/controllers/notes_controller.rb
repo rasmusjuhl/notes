bc180df4cacd0da6d18061108d77355bc0693ec3
Rasmus Juhl <rasmusjuhl@gmail.com>
2016-02-09 12:31:04 +0100

    Working towards a working new view and controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,5 +2,11 @@ class NotesController < ApplicationController
   def index
   end
   def new
+    @note = Note.new
+  end
+  def create
+    note = Note.new(params[:note])
+    note.save
+
   end
 end
