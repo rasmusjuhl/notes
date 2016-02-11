4a16d14eb14df5739c016481f462dd836f3c1b63
Silas <Silas_Jeppe@hotmail.com>
2016-02-11 12:02:36 +0100

    Added NotNullable to the migration file

--- db/migrate/20160209094040_create_notes.rb
+++ db/migrate/20160209094040_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
-      t.string :title
+      t.string :title, :null => false
       t.text :text
 
       t.timestamps null: false
