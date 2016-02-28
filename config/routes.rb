3e0f9cbfbe4bcb108f975fbe278e8d830b6ea905
Silas <Silas_Jeppe@hotmail.com>
2016-02-28 16:00:27 +0100

    static-pages controller added, created static about page and menu navigation added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get '/about/', to: 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
