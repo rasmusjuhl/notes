0551c7a97d19364f7b17fd8dccff2b9ddd17e2d9
Silas <Silas_Jeppe@hotmail.com>
2016-03-03 10:23:36 +0100

    added a edit method for notes, moved topmenu and footer into more global files

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,15 +2,18 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
   def new
     @note = Note.new
   end
+
   def create
     note = Note.new(note_params)
     if(note.save)
       redirect_to notes_url
     end
   end
+
   def show
     @note = Note.find(params[:id])
   end
@@ -19,6 +22,20 @@ class NotesController < ApplicationController
     note = Note.find(params[:id])
     note.destroy
     redirect_to notes_url
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update_attributes(note_params)
+      #Handle a succesful update
+      redirect_to notes_url
+    else
+      render 'edit'
+    end
   end
 
   private
