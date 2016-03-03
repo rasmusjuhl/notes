2fc8609141d3c63f50d60735d9c78d5d706d2562
Rasmus Juhl <rasmusjuhl@gmail.com>
2016-03-03 10:42:29 +0100

    Corrected unicorn conf and added secrets

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,9 +1,9 @@
 root = "/home/deploy/rails/current"
 working_directory root
-pid "/var/run/unicorn.pid"
+pid "/tmp/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/var/run/unicorn.sock"
+listen "/tmp/unicorn.sock"
 worker_processes 2
 timeout 30
